84f3928ba93d0790f7dc80458c550a3d4cfda181
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2023-10-16 08:32:24 -0700

    first commit

---
+++ src/app/Sections/Heropage/Heropag.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Heropag() {
+  return (
+    <div className="hero h-screen">
+      <div className="container mx-auto">
+         <h1>hehehehe</h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Heropag from "./Sections/Heropage/Heropag";
+
+
+export default function Home() {
+  return (
+    <main>
+       <Heropag />
+    </main>
+  )
+}
