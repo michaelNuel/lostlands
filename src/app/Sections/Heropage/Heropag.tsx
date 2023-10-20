004b8a7debe743ef062d28a34ca248b3c1ed9c5e
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2023-10-20 06:46:02 +0000

    ,,

--- src/app/Sections/Heropage/Heropag.tsx
+++ src/app/Sections/Heropage/Heropag.tsx
@@ -2,7 +2,13 @@ export default function Heropag() {
   return (
     <div className="hero h-screen ">
       <div className="container mx-auto">
-         <h1 className="text-red-800">h</h1>
+         <div className="m-auto ">
+            <div className="m-auto ">
+              <div className="m-auto align-middle ">
+                <h1>Your Property In the Metaverse</h1>
+              </div>
+            </div>
+         </div>
       </div>
     </div>
     
